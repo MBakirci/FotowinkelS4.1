b55e2d7eff67b8babcbffa73e6fc48af871cef89
unknown <coen.smulders@student.fontys.nl>
2015-06-16 20:22:49 +0200

    verkoopstatestieken + merge

--- WebApplication3/build/web/testVerkoopstat.jsp
+++ WebApplication3/build/web/testVerkoopstat.jsp
@@ -1,3 +1,4 @@
+        <%@page import="Test.StaticValues"%>
 <%-- 
     Document   : testVerkoopstat
     Created on : 10-Jun-2015, 14:14:59
@@ -16,7 +17,11 @@
     <body>
         <%Calendar cal = Calendar.getInstance();
         cal.set(2015, 6, 20);
-            VerkoopStatestieken.MaandVerkoop(cal);%>
+        VerkoopStatestieken.MaandVerkoop(cal);
+        VerkoopStatestieken.JaarVerkoop(cal);
+        VerkoopStatestieken.FotograafMaandVerkoop(cal, "Henk@yolo.nl");
+        VerkoopStatestieken.FotograafJaarVerkoop(cal, "Henk@yolo.nl");
+        %>
         <h1>Hello World!</h1>
     </body>
 </html>
--- WebApplication3/src/java/Test/VerkoopStatestieken.java
+++ WebApplication3/src/java/Test/VerkoopStatestieken.java
@@ -11,6 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 
 /**
@@ -19,25 +20,23 @@ import java.util.HashMap;
  */
 public class VerkoopStatestieken {
     public static HashMap<String, Object> MaandVerkoop(Calendar maand) throws ClassNotFoundException, InstantiationException, SQLException, IllegalAccessException{
-        Calendar firstDay = maand;
-        Calendar lastDay = maand;
-        firstDay.set(Calendar.DAY_OF_MONTH, maand.getActualMinimum(Calendar.DAY_OF_MONTH));
-        lastDay.set(Calendar.DAY_OF_MONTH, maand.getActualMaximum(Calendar.DAY_OF_MONTH));
-        HashMap<String, Object> result;
-        result = Verkoop(firstDay, lastDay);
-        return result;
+        Calendar firstDay = new GregorianCalendar();
+        Calendar lastDay = new GregorianCalendar();
+        firstDay.set(maand.get(maand.YEAR), maand.get(maand.MONTH)-1, 1);
+        lastDay.set(maand.get(maand.YEAR), maand.get(maand.MONTH)-1, maand.getInstance().getActualMaximum(maand.DAY_OF_MONTH));
+        System.out.print("firstday = " + firstDay.getTime().toString());
+        System.out.print("lastday = " +lastDay.getTime().toString());
+        return Verkoop(firstDay, lastDay);
     }
     
     public static HashMap<String, Object> JaarVerkoop(Calendar jaar) throws ClassNotFoundException, InstantiationException, InstantiationException, SQLException, IllegalAccessException{
-        Calendar firstDay = jaar;
-        Calendar lastDay = jaar;
-        firstDay.set(Calendar.DAY_OF_MONTH, 1);
-        lastDay.set(Calendar.DAY_OF_MONTH, 31);
-        firstDay.set(Calendar.MONTH, 1);
-        lastDay.set(Calendar.MONTH, 12);
-        HashMap<String, Object> result;
-        result = Verkoop(firstDay, lastDay);
-        return result;
+        Calendar firstDay = new GregorianCalendar();
+        Calendar lastDay = new GregorianCalendar();
+        firstDay.set(jaar.get(jaar.YEAR), 0, 1);
+        lastDay.set(jaar.get(jaar.YEAR), 11, 31);
+        System.out.print("firstday = " + firstDay.getTime().toString());
+        System.out.print("lastday = " + lastDay.getTime().toString());
+        return Verkoop(firstDay, lastDay);
     }
     
     public static HashMap<String, Object> Verkoop(Calendar StartDate, Calendar EndDate) throws ClassNotFoundException, ClassNotFoundException, InstantiationException, SQLException, SQLException, IllegalAccessException{
@@ -49,10 +48,9 @@ public class VerkoopStatestieken {
                 String querry = "Select sum(pf.AANTAL) as items, sum(pf.PRIJS*pf.AANTAL) as omzet, sum(pf.PRIJS*pf.AANTAL*(pf.BTW/100)) as btw "
                         + "From FW_Bestelling b, FW_PRODUCT_FOTO pf "
                         + "Where b.BESTELLINGID = pf.FK_BESTELLINGID "
-                        + "and b.BESTELDATUM between :? and :?";
+                        + "and b.BESTELDATUM between ? and ?";
                 state = ts.conn.prepareStatement(querry);
-                java.sql.Date d = ToSQLDate(StartDate);
-                state.setDate(1, d);
+                state.setDate(1, ToSQLDate(StartDate));
                 state.setDate(2, ToSQLDate(EndDate));
                 ResultSet result = state.executeQuery();
                 if(result.next()){
@@ -71,25 +69,76 @@ public class VerkoopStatestieken {
         /*Select sum(pf.AANTAL) as items, sum(pf.PRIJS*pf.AANTAL) as omzet, sum(pf.PRIJS*pf.AANTAL*(pf.BTW/100)) as btw
         From FW_Bestelling b, FW_PRODUCT_FOTO pf
         Where b.BESTELLINGID = pf.FK_BESTELLINGID
-        and b.BESTELDATUM between :? and :?*/
+        and b.BESTELDATUM between ? and ?*/
         return null;
     }
     
-    public static HashMap<String, Object> FotograafMaandVerkoop(User user){
-        HashMap<String, Object> result = FotograafMaandVerkoop(user.geteMail());
-        return result;
+    public static HashMap<String, Object> FotograafMaandVerkoop(Calendar maand, User user){
+        return FotograafMaandVerkoop(maand, user.geteMail());
     }
     
-    public static HashMap<String, Object> FotograafMaandVerkoop(String email){
-        return null;
+    public static HashMap<String, Object> FotograafMaandVerkoop(Calendar maand, String email){
+        Calendar firstDay = new GregorianCalendar();
+        Calendar lastDay = new GregorianCalendar();
+        firstDay.set(maand.get(maand.YEAR), maand.get(maand.MONTH)-1, 1);
+        lastDay.set(maand.get(maand.YEAR), maand.get(maand.MONTH)-1, maand.getInstance().getActualMaximum(maand.DAY_OF_MONTH));
+        System.out.print("firstday = " + firstDay.getTime().toString());
+        System.out.print("lastday = " +lastDay.getTime().toString());
+        return FotograafVerkoop(firstDay, lastDay, email);
     }
     
-    public static HashMap<String, Object> FotograafJaarVerkoop(User user){
-        HashMap<String, Object> result = FotograafJaarVerkoop(user.geteMail());
-        return result;
+    public static HashMap<String, Object> FotograafJaarVerkoop(Calendar jaar, User user){
+        return FotograafJaarVerkoop(jaar, user.geteMail());
     }
     
-    public static HashMap<String, Object> FotograafJaarVerkoop(String email){
+    public static HashMap<String, Object> FotograafJaarVerkoop(Calendar jaar, String email){
+        Calendar firstDay = new GregorianCalendar();
+        Calendar lastDay = new GregorianCalendar();
+        firstDay.set(jaar.get(jaar.YEAR), 0, 1);
+        lastDay.set(jaar.get(jaar.YEAR), 11, 31);
+        System.out.print("firstday = " + firstDay.getTime().toString());
+        System.out.print("lastday = " + lastDay.getTime().toString());
+        return FotograafVerkoop(firstDay, lastDay, email);
+    }
+    
+    public static HashMap<String, Object> FotograafVerkoop(Calendar StartDate, Calendar EndDate, String email){
+        HashMap<String, Object> stats = new HashMap<>();
+        Databaseconnector ts = new Databaseconnector();
+        try {
+            if(ts.verbindmetDatabase()){
+                PreparedStatement state = null;
+                String querry = "Select sum(pf.AANTAL) as items, sum(pf.PRIJS*pf.AANTAL) as omzet, sum(pf.PRIJS*pf.AANTAL*(pf.BTW/100)) as btw " +
+                        "From FW_Bestelling b, FW_PRODUCT_FOTO pf , FW_FOTO f, FW_ACCOUNT a " +
+                        "Where b.BESTELLINGID = pf.FK_BESTELLINGID " +
+                        "and f.UNIQUECODE = pf.FOTOCODE " +
+                        "and a.ACCOUNT_ID = f.FOTOGRAAFID " +
+                        "and b.BESTELDATUM between ? and ? " +
+                        "and a.EMAIL = ?";
+                state = ts.conn.prepareStatement(querry);
+                state.setDate(1, ToSQLDate(StartDate));
+                state.setDate(2, ToSQLDate(EndDate));
+                state.setString(3, email);
+                ResultSet result = state.executeQuery();
+                if(result.next()){
+                    stats.put("items", result.getInt("items"));
+                    stats.put("omzet", result.getInt("omzet"));
+                    stats.put("btw", result.getInt("btw"));
+                }
+                return stats;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            ts.close();
+        }
+        
+        /*Select sum(pf.AANTAL) as items, sum(pf.PRIJS*pf.AANTAL) as omzet, sum(pf.PRIJS*pf.AANTAL*(pf.BTW/100)) as btw 
+        From FW_Bestelling b, FW_PRODUCT_FOTO pf , FW_FOTO f, FW_ACCOUNT a
+        Where b.BESTELLINGID = pf.FK_BESTELLINGID
+        and f.UNIQUECODE = pf.FOTOCODE
+        and a.ACCOUNT_ID = f.FOTOGRAAFID
+        and b.BESTELDATUM between ? and ?
+        and a.EMAIL = ?;*/
         return null;
     }
     
